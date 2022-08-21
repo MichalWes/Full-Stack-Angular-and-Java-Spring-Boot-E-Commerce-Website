ab544cd11265ec71db28f7e482fd49a97ce7c767
MichalWes <massive7m@gmail.com>
2022-08-21 22:13:11 +0200

    defining variable

---
+++ typescript training/02-sample-types.js
@@ -0,0 +1,9 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Andrzej";
+var lastName = "Rafał";
+console.log(found);
+console.log("The grade is " + grade);
+console.log("The grade is ".concat(grade));
+console.log("Hi " + firstName + " " + lastName);
+console.log("Hi ".concat(firstName, " ").concat(lastName));
---
+++ typescript training/02-sample-types.ts
@@ -0,0 +1,10 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Andrzej";
+let lastName: string = "Rafał";
+
+console.log(found);
+console.log("The grade is " + grade);
+console.log(`The grade is ${grade}`);
+console.log("Hi " + firstName + " " + lastName);
+console.log(`Hi ${firstName} ${lastName}`);
