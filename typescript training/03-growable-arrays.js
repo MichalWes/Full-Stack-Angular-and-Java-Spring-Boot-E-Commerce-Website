638cd6cbc3caee983f562ff1872a0007adab28c9
Michał Węsierski <mwesierski@sii.pl>
2022-08-22 09:05:13 +0200

    loops and arrays 2

---
+++ typescript training/03-growable-arrays.js
@@ -0,0 +1,4 @@
+var sportsTwo = ["Golf", "Cricket", "Tennis"];
+sportsTwo.push("Baseball");
+sportsTwo.push("Futbol");
+sportsTwo.forEach(function (sport) { return console.log(sport); });
---
+++ typescript training/03-growable-arrays.ts
@@ -0,0 +1,6 @@
+let sportsTwo: string[] = ["Golf", "Cricket", "Tennis"];
+
+sportsTwo.push("Baseball");
+sportsTwo.push("Futbol");
+
+sportsTwo.forEach((sport) => console.log(sport));
